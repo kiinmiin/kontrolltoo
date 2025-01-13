197dea8efcbd1a90e50e76e85e78c049e30f344a
kiinmiin <rasmo.kiin@voco.ee>
2025-01-13 16:07:08 +0200

    Viies ülesanne lahendatud

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,14 +1,16 @@
 import React from "react"
 
 const MealItem = (props) => {
+    const price = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(props.meal.price)
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3>{props.meal.name}</h3>
-                    <p>{props.meal.price}</p>
-                    <p>{props.meal.description}</p>
+                    <p className="meal-item-price">{price}</p>
+                    <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p>
                     <button >Add to Cart</button>
