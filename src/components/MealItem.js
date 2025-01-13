f80d3c7e1bd3221acf566758cd000f64ac525083
kiinmiin <rasmo.kiin@voco.ee>
2025-01-13 14:09:10 +0200

    Ülesanne kolm lahendatud

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,3 +1,5 @@
+import React from "react"
+
 const MealItem = (props) => {
     return (
         <li>
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,8 +1,19 @@
-const Meals = () => {
+import { useEffect, useState } from "react"
+
+const Meals = (props) => {
+    const [meals, setMeals] = useState(null); 
+    
+    useEffect(() => {
+        fetch('http://localhost:3001/meals')
+        .then(res => res.json())
+        .then(meal => setMeals(meal))
+    })
+
+    console.log(meals)
     return (
         <ul id="meals">
             { 
-                <p>Create list of meals, using fetch data from backend</p>// list of meals
+                //<li><MealItem /></li>
             }
         </ul>
     )
